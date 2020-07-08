95ee956138d93c6d1d53154ec9f717731dc47bcb
okmasa <bianconerimura@gmail.com>
2020-07-08 13:00:39 +0900

    Update page title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | サプリアプリ"
   end
 
 end
