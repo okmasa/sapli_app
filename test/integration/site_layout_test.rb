eb7f7e711946c81e70766cd868c3c31874cae634
okmasa <bianconerimura@gmail.com>
2020-07-09 13:34:50 +0900

    Make users controller

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,8 +5,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", root_path, count: 3
     assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", signup_path, count: 2
   end
 
 end
