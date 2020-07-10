6db3f4a9dff3fceac1b4b8e907864bd409bb7a41
okmasa <bianconerimura@gmail.com>
2020-07-10 17:03:50 +0900

    Finish user signup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,13 +5,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Home | サプリアプリ"
+    assert_select "title", "ホーム | サプリアプリ"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "About | サプリアプリ"
+    assert_select "title", "使い方 | サプリアプリ"
   end
 
 
