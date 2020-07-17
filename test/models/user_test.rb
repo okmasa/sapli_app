b7b3a192c8e1c41f8d8a555af9da57ae5c786750
okmasa <bianconerimura@gmail.com>
2020-07-17 19:01:39 +0900

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -84,4 +84,32 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test "should follow and unfollow a user" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    # assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
+
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    lana    = users(:lana)
+  #フォローしているユーザの投稿があること
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    #自分の投稿があること
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    #フォローしていないユーザの投稿が無いこと
+    archer.microposts.each do |post_unfollowed|
+      assert_not  michael.feed.include?(post_unfollowed)
+    end
+  end
 end
