daf2cf7b703d550fd2eaef41672eb2da644c3066
okmasa <bianconerimura@gmail.com>
2020-08-17 10:23:23 +0900

    Implement new index

--- test/models/favorite_test.rb
+++ test/models/favorite_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class FavoriteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @favorite = Favorite.new(user_id: users(:michael).id,
+                             supplement_id: supplements(:vitaminC).id)
+  end
+
+  test "should be valid" do
+    assert @favorite.valid?
+  end
+
+  test "should require user_id" do
+    @favorite.user_id =nil
+    assert_not @favorite.valid?
+  end
+
+  test "should require supplement_id" do
+    @favorite.supplement_id = nil
+    assert_not @favorite.valid?
+  end
+
 end
