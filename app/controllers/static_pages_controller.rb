21da13823c3b088db179bcccbe408f482a87e432
okmasa <bianconerimura@gmail.com>
2020-08-30 18:42:51 +0900

    Add About page and Fix microposts image

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,7 @@ class StaticPagesController < ApplicationController
       @user = current_user
       @feed_items = current_user.feed.paginate(page: params[:page], per_page: 3 )
       @reviews = current_user.reviews.paginate(page: params[:page], per_page: 3 )
-      @favorite_supplements = @user.favorite_supplements
+      @favorite_supplements = current_user.favorite_supplements
     end
     # ランキング機能
     # @ranks = Supplement.find(Review.group(:supplement_id).average(:score).order('average(:score) desc').take(3).pluck(:supplement_id))
