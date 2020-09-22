0031b39ab7d6ffabed10ae56533e967c68880896
okmasa <bianconerimura@gmail.com>
2020-09-22 17:30:17 +0900

    change favorite ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,21 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.build(supplement_id: params[:supplement_id])
     favorite.save
     @supplement = Supplement.find_by(id: params[:supplement_id])
-    redirect_to supplement_path(@supplement)
+    # redirect_to supplement_path(@supplement)
+    respond_to do |format|
+      format.html { redirect_to supplement_path(@supplement) }
+      format.js
+    end
   end
   
   def destroy
     favorite = Favorite.find_by(supplement_id: params[:supplement_id], user_id: current_user.id)
     favorite.destroy
     @supplement = Supplement.find_by(id: params[:supplement_id])
-    redirect_to supplement_path(@supplement)
+    # redirect_to supplement_path(@supplement)
+    respond_to do |format|
+      format.html { redirect_to supplement_path(@supplement) }
+      format.js
+    end
   end
 end
