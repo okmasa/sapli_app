dd4848a54476052ed07b87bdd87d409367bda9ae
okmasa <bianconerimura@gmail.com>
2020-11-22 16:02:17 +0900

    delete cache

--- db/migrate/20200723101429_add_description_to_supplements.rb
+++
@@ -1,5 +0,0 @@
-class AddDescriptionToSupplements < ActiveRecord::Migration[6.0]
-  def change
-    add_column :supplements, :description, :text
-  end
-end
--- db/migrate/20200726065947_change_data_type_total_score_of_supplements.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDataTypeTotalScoreOfSupplements < ActiveRecord::Migration[6.0]
-  def change
-    change_column :supplements, :total_score, :float
-  end
-end
