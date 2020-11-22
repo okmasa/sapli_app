4bdf8ae5e070407330329987ab86e749b3fb4afc
okmasa <bianconerimura@gmail.com>
2020-11-22 16:52:42 +0900

    delete cache

--- db/migrate/20200718031941_add_img_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddImgToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :img, :string, default: nil
-  end
-end
--- db/migrate/20200726062304_change_data_total_score_to_supplements.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDataTotalScoreToSupplements < ActiveRecord::Migration[6.0]
-  def change
-    change_column :supplements, :total_score, :float
-  end
-end
